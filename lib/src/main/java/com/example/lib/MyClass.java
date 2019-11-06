44f651afe793af0580b671a735c1463dfc0b811c
youht384 <karta2637781@gmail.com>
2019-11-06 21:51:06 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,13 @@
+package com.example.lib;
+
+public class MyClass {
+    public static void main(String[] avg) {
+        int i,j;
+        for(i=1;i<=9;i++){
+            for(j=1;j<=9;j++){
+                System.out.print(i+"X"+j+"="+i*j+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
